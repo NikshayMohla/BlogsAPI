29a646b35f319f813a9011132ddb620f4c532cf2
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-21 14:02:44 +0530

    Comment Controller Updated

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,24 +1,31 @@
-const Post = require("../models/postModel")
-const Comment = require("../models/commentModel")
-const mongoose = require("mongoose")
-
+const Post = require("../models/postModel");
+const Comment = require("../models/commentModel");
 
 exports.createComment = async (req, res) => {
     try {
-        const { post, user, body } = req.body
-        const comment = new Comment(
+        //fetch data from req body 
+        const { post, user, body } = req.body;
+        //create a comment object
+        const comment = new Comment({
             post, user, body
-        )
+        });
 
-        const savedComment = await comment.save()
-        const updatedPost = await Post.findByIdAndUpdate(post, { $push: { comments: savedComment._id } }, { new: true }).populate("comments").exec()
+        //save the new comment into the database
+        const savedComment = await comment.save();
+
+        //find the post by ID, add the new commnet to its comments array
+        const udpatedPost = await Post.findByIdAndUpdate(post, { $push: { comments: savedComment._id } }, { new: true })
+            .populate("comments") //populate the comments array with comment documents
+            .exec();
 
         res.json({
-            post: updatedPost,
-        })
-    }
-    catch (e) {
-        console.error(e)
+            post: udpatedPost,
+        });
 
     }
-}
+    catch (error) {
+        return res.status(500).json({
+            error: "Error While Creating comment",
+        });
+    }
+};
--- routes/blog.js
+++ routes/blog.js
@@ -1,3 +1,10 @@
 const express = require("express")
 
 const router = express.Router()
+
+const { createComment } = require("../controllers/commentController")
+
+
+router.post("/comments/create", createComment)
+
+module.exports = router
